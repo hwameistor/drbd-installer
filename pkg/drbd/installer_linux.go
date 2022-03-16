8ae280e5529868c2152fcab5b47ed5e23d4be0b0
Niu Lechuan <lechuan.niu@daocloud.io>
2022-03-16 15:13:01 +0800

    add ensure DRBD autoload when OS restart
    Signed-off-by: Niu Lechuan <lechuan.niu@daocloud.io>

--- pkg/drbd/installer_linux.go
+++ pkg/drbd/installer_linux.go
@@ -17,6 +17,24 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
+const (
+	DRBDAutoloaderFile = "/etc/sysconfig/modules/drbd.modules"
+	DRBDAutoloader     = `#!/bin/sh
+/sbin/modinfo drbd > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+    /sbin/modprobe drbd
+fi
+
+/sbin/modinfo drbd_transport_tcp > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+    /sbin/modprobe drbd_transport_tcp
+fi`
+	LibModulesPathTemplate      = "/lib/modules/%s/extra/drbd90"
+	DRBDPathInContainerTemplate = "/kernel-mods/drbd/%s/%s/%s/%s"
+	DepmodCMD                   = "depmod"
+	ModprobeCMD                 = "modprobe"
+)
+
 type DRBDKernelModInstaller struct {
 	OS,
 	Arch,
@@ -37,8 +55,8 @@ func NewDRBDKernelModInstaller() (*DRBDKernelModInstaller, error) {
 		return nil, err
 	}
 
-	installer.KernelModToHostPath = strings.ToLower(fmt.Sprintf("/lib/modules/%s/extra/drbd90", installer.KernelVersionReleaseOriginString))
-	installer.KernelModSourcePath = strings.ToLower(fmt.Sprintf("/kernel-mods/drbd/%s/%s/%s/%s", installer.OS, installer.KernelVersion, installer.KernelRelease, installer.Arch))
+	installer.KernelModToHostPath = strings.ToLower(fmt.Sprintf(LibModulesPathTemplate, installer.KernelVersionReleaseOriginString))
+	installer.KernelModSourcePath = strings.ToLower(fmt.Sprintf(DRBDPathInContainerTemplate, installer.OS, installer.KernelVersion, installer.KernelRelease, installer.Arch))
 
 	log.Infof("host OS: %s", installer.OS)
 	log.Infof("host CPU arch: %s", installer.Arch)
@@ -95,7 +113,7 @@ func (i *DRBDKernelModInstaller) CopyKernelModToHost() error {
 
 func (i *DRBDKernelModInstaller) Depmod() error {
 	cmd := exechelper.ExecParams{
-		CmdName: "depmod",
+		CmdName: DepmodCMD,
 		Timeout: 300,
 	}
 
@@ -116,7 +134,7 @@ func (i *DRBDKernelModInstaller) Modprobe() error {
 	for _, file := range files {
 		modName := strings.TrimSuffix(file.Name(), filepath.Ext(file.Name()))
 		cmd := exechelper.ExecParams{
-			CmdName: "modprobe",
+			CmdName: ModprobeCMD,
 			CmdArgs: []string{modName},
 		}
 
@@ -128,21 +146,6 @@ func (i *DRBDKernelModInstaller) Modprobe() error {
 		log.Infof("%s has being successfully installed on host", modName)
 	}
 	return nil
-}
-
-func (i *DRBDKernelModInstaller) EnsureAutoLoadWhenHostRestarted() error {
-	return nil
-}
-
-func int8ToStr(arr []int8) string {
-	b := make([]byte, 0, len(arr))
-	for _, v := range arr {
-		if v == 0x00 {
-			break
-		}
-		b = append(b, byte(v))
-	}
-	return string(b)
 }
 
 func (i *DRBDKernelModInstaller) parseKernelVersionAndRelease() error {
@@ -165,4 +168,48 @@ func (i *DRBDKernelModInstaller) parseKernelVersionAndRelease() error {
 	i.KernelVersionReleaseOriginString = versionReleaseStr
 
 	return nil
+}
+
+func (i *DRBDKernelModInstaller) EnsureAutoLoadWhenHostRestarted() error {
+	exists, err := isFileExists(DRBDAutoloaderFile)
+	if err != nil {
+		return err
+	}
+	if exists {
+		return nil
+	}
+
+	autoloader, err := os.Create(DRBDAutoloaderFile)
+	if err != nil {
+		return err
+	}
+	defer autoloader.Close()
+
+	if _, err := autoloader.Write([]byte(DRBDAutoloader)); err != nil {
+		return err
+	}
+	return nil
+}
+
+func isFileExists(path string) (bool, error) {
+	file, err := os.Open(path)
+	if err != nil && os.IsNotExist(err) {
+		return false, nil
+	} else if err != nil {
+		return false, err
+	}
+	defer file.Close()
+
+	return true, nil
+}
+
+func int8ToStr(arr []int8) string {
+	b := make([]byte, 0, len(arr))
+	for _, v := range arr {
+		if v == 0x00 {
+			break
+		}
+		b = append(b, byte(v))
+	}
+	return string(b)
 }
